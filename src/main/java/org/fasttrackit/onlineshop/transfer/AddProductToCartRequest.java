cc9c25c1b886c105db09282adb2bb9807759bc38
Andrei <andreibuzas@gmail.com>
2020-09-24 21:34:14 +0300

    studied one to one relationship; added cart entity

---
+++ src/main/java/org/fasttrackit/onlineshop/transfer/AddProductToCartRequest.java
@@ -0,0 +1,35 @@
+package org.fasttrackit.onlineshop.transfer;
+
+import javax.validation.constraints.NotNull;
+
+public class AddProductToCartRequest {
+
+    @NotNull
+    private Long userID;
+    @NotNull
+    private Long productID;
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public Long getProductID() {
+        return productID;
+    }
+
+    public void setProductID(Long productID) {
+        this.productID = productID;
+    }
+
+    @Override
+    public String toString() {
+        return "AddProductToCartRequest{" +
+                "userID=" + userID +
+                ", productID=" + productID +
+                '}';
+    }
+}
