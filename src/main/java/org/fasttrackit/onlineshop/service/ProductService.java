0e80ac84f13981ad2f22dfd249a5de09a284a904
Andrei <andreibuzas@gmail.com>
2020-10-08 20:48:17 +0300

    added product controller and studied query by example

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -3,12 +3,17 @@ package org.fasttrackit.onlineshop.service;
 import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistence.ProductRepository;
+import org.fasttrackit.onlineshop.transfer.product.GetProductsRequest;
 import org.fasttrackit.onlineshop.transfer.product.ProductResponse;
 import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -43,7 +48,39 @@ public class ProductService {
                 .orElseThrow(() -> new ResourceNotFoundException("Product " + id + " does not exist"));
     }
 
+    public ProductResponse getProductResponse(long id) {
+        Product product = getProduct(id);
+        return mapProductResponse(product);
+    }
 
+    public Page<ProductResponse> getProducts(GetProductsRequest request, Pageable pageable) {
+        LOGGER.info("Retrieving products: {}", request);
+
+        Product exampleProduct = new Product();
+        exampleProduct.setName(request.getPartialName());
+        exampleProduct.setDescription(request.getPartialDescription());
+        exampleProduct.setQuantity(request.getMinQuantity());
+        exampleProduct.setPrice(request.getPrice());
+        exampleProduct.setCarts(null);
+
+        Example<Product> example = Example.of(exampleProduct,
+                ExampleMatcher.matchingAny()
+                        .withMatcher("name", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
+                        .withMatcher("description", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase()));
+        //todo: add matcher for minimum quantity
+
+        //query by example
+        Page<Product> productsPage = productRepository.findAll(example, pageable);
+
+        List<ProductResponse> productDTOs = new ArrayList<>();
+
+        for(Product product: productsPage.getContent()) {
+            ProductResponse productResponse = mapProductResponse(product);
+            productDTOs.add(productResponse);
+        }
+
+        return new PageImpl<>(productDTOs, pageable, productsPage.getTotalElements());
+    }
 
     private ProductResponse mapProductResponse(Product product) {
         ProductResponse productResponse = new ProductResponse();
--- src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
+++ src/main/java/org/fasttrackit/onlineshop/web/ProductController.java
@@ -1,10 +1,12 @@
 package org.fasttrackit.onlineshop.web;
 
-import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.transfer.product.GetProductsRequest;
 import org.fasttrackit.onlineshop.transfer.product.ProductResponse;
 import org.fasttrackit.onlineshop.transfer.product.SaveProductRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,9 +32,15 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable long id) {
-        Product product = productService.getProduct(id);
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable long id) {
+        ProductResponse product = productService.getProductResponse(id);
         return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ProductResponse>> getProducts(GetProductsRequest request, Pageable pageable) {
+        Page<ProductResponse> products = productService.getProducts(request, pageable);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
 }
